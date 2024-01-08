10eb7dcf266abc00f42e2b06f506f2f29058a3a0
77sarvar77 <sarvar.usmonov.77@mail.ru>
2024-01-08 22:25:14 +0500

    js

---
+++ apps.js
@@ -0,0 +1,59 @@
+                        // MALUMOT TURLARI
+
+//Number
+// let number = 7.7;
+// console.log(8 / 4);
+// console.log(8 / 0);// Infinity
+// console.log(-8 / 0);// -Infinity
+// console.log("ROG" + 7);//NaN
+
+// let = {Bu o'zgarishga ega}
+// const ={Bu o'zgarishga ega emas}
+
+//String
+// const gamerName = "WoT Blitz Gamer";
+
+//Boolean
+// const arePlay = true or false;
+
+//Null "O'zgaruvchini o'zi yoq"
+// console.log(gamerAge);
+
+//Undefiled
+// let game;
+// console.log(game);
+
+//Object
+// const thief = {
+//     age: 15;
+//     tall: 180;
+//     weight: 50;
+// };
+                        // FOYDALANOVCHI BILAN ODDIY MULOQOT
+
+// alert("gaming");
+
+// const feel = confirm("Whats up my lovely guys? Please give me answer!");
+// console.log(feel);
+
+//  const feel = confirm("Whats up my lovely guys? Please give me answer!");
+//  console.log(typeof feel);
+
+// const curretplace = prompt("Where are you now? Please give me answer!");
+// console.log(curretplace);
+
+// const curretplace = prompt("Where are you now? Please give me answer!");
+// console.log(typeof curretplace);
+
+// const age = prompt("How old are you?", "18");
+// console.log(age + 10);
+
+// const age = +prompt("How old are you?", "18");
+// console.log(age + 10);
+
+// const favouriteGame = [];
+
+// favouriteGame[0] = prompt("What's your favourite game #1 ?", "");
+// favouriteGame[1] = prompt("What's your favourite game #2 ?", "");
+// favouriteGame[2] = prompt("What's your favourite game #3 ?", "");
+// console.log(favouriteGame);             
